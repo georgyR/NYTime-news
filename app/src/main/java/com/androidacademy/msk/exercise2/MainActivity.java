1c1f1ddbdfd27de526dae3c2c6b72b520d0ebc85
georgy.ryabykh <georgy.zoomeee@gmail.com>
2018-09-26 15:05:43 +0300

    redesigned layout, deleted unused dependences

--- app/src/main/java/com/androidacademy/msk/exercise2/MainActivity.java
+++ app/src/main/java/com/androidacademy/msk/exercise2/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
